0ad6d0be6f9c12101f18e6b51d597782af3549b3
joanita-51 <nakityojoanita51@gmail.com>
2024-01-28 00:29:12 +0300

    basic commands

---
+++ server.js
@@ -0,0 +1,92 @@
+//create an express application
+const express = require('express')
+const app = express();
+const db = require('./database.js')
+var md5 = require("md5")
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//port the application is to be running
+
+const PORT = 4000
+
+//listen
+app.listen(PORT,()=>{console.log(`The app is running on port ${PORT} `)})
+
+
+//Root endpoint
+app.get("/greetings", (req, res)=>{
+    res.send('Hello')
+})
+
+app.get("/", function(req, res){
+    res.json({"message":"ok"})
+})
+
+//Getting a list of users
+//app.method('',()=>{})
+
+app.get("/api/users", (req, res, next) => {
+    var sql = "select * from user"
+    var params = []
+    db.all(sql, params, (err, rows) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":rows
+        })
+      });
+});
+
+//get single user by id
+app.get("/api/user/:id", (req, res, next) => {
+    var sql = "select * from user where id = ?"
+    var params = [req.params.id]
+    db.get(sql, params, (err, row) => {
+        if (err) {
+          res.status(400).json({"error":err.message});
+          return;
+        }
+        res.json({
+            "message":"success",
+            "data":row
+        })
+      });
+});
+
+//create a new user
+app.post("/api/user/", (req, res, next) => {
+    var errors=[]
+    if (!req.body.password){
+        errors.push("No password specified");
+    }
+    if (!req.body.email){
+        errors.push("No email specified");
+    }
+    if (errors.length){
+        res.status(400).json({"error":errors.join(",")});
+        return;
+    }
+    var data = {
+        name: req.body.name,
+        email: req.body.email,
+        password : md5(req.body.password)
+    }
+    var sql ='INSERT INTO user (name, email, password) VALUES (?,?,?)'
+    var params =[data.name, data.email, data.password]
+    db.run(sql,params, function (err) {
+        if (err){
+            res.status(400).json({"error": err.message})
+            return;
+        }
+        res.json({
+            "message": "success",
+            "data": data,
+            "id" : this.lastID
+        })
+    });
+})
